bc3e8d6bd9e1e5cfd4aa678f2b24941fe80f96f0
Barak Oshri <barakoshri@DN51vsfo.SUNet>
2015-05-28 21:23:10 -0700

    Completed run.py

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -4,64 +4,60 @@ import numpy as np
 from attrdict import AttrDict
 import sys
 
-def corrupt(input):
+#def corrupt(input):
 
 
 class AutoEncoder:
 	# For Word Vectors Only. Input is a two dimensional array, with input[i] a list of integers accessing  
-	def __init__(self, wordVectors, n_hidden=500, encoder='RNN', encoder_params=None, decoder_params=None):
+	def __init__(self, encoder_params, decoder_params, wordVectors, end_token, input=None, n_hidden=500, encoder='RNN', f='sigmoid'):
 
 		self.n_hidden = n_hidden
+		self.end_token = end_token
 
 		# Decoder Parameters
-		if decoder_params is None:
-			self.params.H = np.ones((n_hidden, n_hidden))
-			self.params.Y = np.ones((n_hidden, len(wordVectors)))
-			self.params.C = np.ones((n_hidden, n_hidden))
-			self.params.S = np.ones((len(wordVectors), n_hidden))
-			self.params.b = np.ones(len(wordVectors))
-		else:
-			self.params = AttrDict(decoder_params)
-
-		self.decoder_params = [self.params.H, self.params.Y, self.params.C, self.params.S, self.params.b]
+		self.params = AttrDict(decoder_params)
+		self.decoder_params = [self.params.H1, self.params.Y, self.params.C, self.params.S, self.params.B]
 
 		# Neuron Choice
-		if neuron == 'sigmoid':
+		if f == 'sigmoid':
 			self.f = T.nnet.sigmoid
-		if neuron == 'tanh':
+		elif f == 'tanh':
 			self.f = T.tanh
-		if neuron == 'gate':
+		elif f == 'gate':
 			pass
 		else:
 			sys.exit('Invalid neuron')
 
 		# Encoder Choice
-		self.encoder = globals()[encoder](wordVectors, params=encoder_params, hdim=n_hidden, f=self.f)
+		self.encoder = globals()[encoder](wordVectors, n_hidden, params=encoder_params, hdim=n_hidden, f=self.f)
 		self.get_hidden_values = self.encoder.get_hidden_values
 
-	def get_reconstructed_input(self, hidden, end_token):
-		[h, s], _ - theano.scan(fn = lambda a, b, c: _decoder_recurrence(a, b, c, end_token), 
-								outputs_info = [hidden, hidden], non_sequences = hidden)
+		if input is None:
+			self.x = T.dmatrix(name='input')
+		else:
+			self.x = input
+
+	def get_reconstructed_input(self, hidden):
+		[h, s], _ - theano.scan(fn = self._decoder_recurrence, outputs_info = [hidden, hidden], non_sequences = hidden)
 		self.s = s
 		self.y = np.argmax(s, axis=1)
 
 		return s
 
-	def _decoder_recurrence(ht_1, yt_1, hidden, end_token):
-		h_t = f(T.dot(self.H, ht_1) + T.dot(self.Y, yt_1) + T.dot(self.C, hidden))
-		s_t = T.nnet.softmax(T.dot(self.S, h_t) + self.b)
+	def _decoder_recurrence(self, ht_1, yt_1, hidden):
+		h_t = f(T.dot(self.H1, ht_1) + T.dot(self.Y, yt_1) + T.dot(self.C, hidden))
+		s_t = T.nnet.softmax(T.dot(self.S, h_t) + self.B)
 
-		return [h_t, s_t], theano.scan_module.until(np.argmax(s_t) == end_token)
+		return [h_t, s_t], theano.scan_module.until(np.argmax(s_t) == self.end_token)
 
-	def get_cost_updates(self, input, end_token, corruption_level, learning_rate, corrupt=False):
-		hidden_input = self.encoder.get_hidden_values(input, corrupt)
-		output = self.get_reconstructed_input(hidden_input, end_token)
+	def get_cost_updates(self, learning_rate):
+		hidden_input = self.encoder.get_hidden_values(input)
+		output = self.get_reconstructed_input(hidden_input)
 		hidden_output = self.encoder.get_hidden_values(output)
 
 		L = np.linalg.norm(hidden_input - hidden_output)
 
 		encoder_decoder_params = self.decoder_params + self.encoder.encoder_params
-
 		gparams = T.grad(L, encoder_decoder_params)
 
 		updates = [(param, param - learning_rate * gparam) for param, gparam in zip(self.params, gparams)]
@@ -76,24 +72,22 @@ class AutoEncoder:
 
 class RNN:
 
-	def __init__(self, wordVectors, params=None, hdim=None, f=None):
-		if params = None:
-			self.params.H = np.ones((hdim, hdim))
-		else:
-			self.params = AttrDict(params)
-
-		self.encoder_params = [self.params.H]
+	def __init__(self, wordVectors, n_hidden, params=None, hdim=None, f=None):
+		self.params = AttrDict(params)
+		self.encoder_params = [self.params.H2]
+		self.h0 = theano.shared(np.zeros((n_hidden), dtype=theano.config.floatX))
+		self.n_hidden = n_hidden
 
 		self.wordVectors = wordVectors
 
-	def _recurrence(x_t, h_tm1):
-		h_t = self.f(T.dot(self.H, h_tm1) + wordVectors[x_t])
+	def _recurrence(self, x_t, h_tm1):
+		h_t = self.f(T.dot(self.H2, h_tm1) + wordVectors[x_t])
 
 		return h_t
 
 	#Input is a list of word vectors
-	def get_hidden_values(self, input, corrupt=False):
-		h, _ = theano.scan(fn=_recurrence, sequences=corrupt(input), outputs_info=self.h0)
+	def get_hidden_values(self, input):
+		h, _ = theano.scan(fn=self._recurrence, sequences=input, outputs_info=self.h0)
 		self.current_hidden = h[-1]
 
 		return self.hidden
--- run.py
+++ run.py
@@ -1,11 +1,42 @@
 import theano
 from theano import tensor as T
 import numpy as np
+from AutoEncoder import *
 
+index = T.lscalar()
+x = T.matrix('x')
 
+wordVectors = np.zeros((20, 5))
+batch_size = 5
+learning_rate = 0.05
+n_hidden = 50
+training_epochs = 3
+n_train_batches = 4
+end_token = 0
 
-da = AutoEncoder(wordVectors, n_hidden = 20)
+decoder_params = {
+	'H1' : theano.shared(value = np.zeros((n_hidden, n_hidden), dtype=theano.config.floatX), borrow=True),
+	'Y' : theano.shared(value = np.zeros((n_hidden, len(wordVectors)), dtype=theano.config.floatX), borrow=True),
+	'C' : theano.shared(value = np.zeros((n_hidden, n_hidden), dtype=theano.config.floatX), borrow=True),
+	'S' : theano.shared(value = np.zeros((len(wordVectors), n_hidden), dtype=theano.config.floatX), borrow=True),
+	'B' : theano.shared(value = np.zeros((len(wordVectors)), dtype=theano.config.floatX), borrow=True)
+}
+encoder_params = {
+	'H2' : theano.shared(value = np.zeros((n_hidden, n_hidden), dtype=theano.config.floatX), borrow=True)
+}
 
-# cost, updates = da.get_cost_updates(corruption_level, learning_rate)
+autoencoder = AutoEncoder(encoder_params, decoder_params, wordVectors, end_token, input = x, n_hidden = n_hidden)
+cost, updates = autoencoder.get_cost_updates(learning_rate = learning_rate)
+train_autoencoder = theano.function([index], cost, updates = updates, givens = {x: X_train[index * batch_size : (index + 1) * batch_size]})
 
-# train_da = theano.function([index], cost, updates=updates, givens={x: train_set_x[index * batch_size: (index + 1) * batch_size]})
+start_time = time.clock()
+for epoch in xrange(training_epochs):
+	c = []
+	for batch_index in xrange(n_train_batches):
+		c.append(train_autoencoder(batch_index))
+
+	print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
+
+end_time = time.clock()
+training_time = (end_time - start_time)
+print training_time / 60.0
