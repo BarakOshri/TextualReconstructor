491252eef44c07b16ae304ceb46fc4431464f487
Barak Oshri <barakoshri@gmail.com>
2015-06-05 15:55:43 -0700

    Started writing test function

--- dA.py
+++ dA.py
@@ -1,26 +1,9 @@
 """
- This tutorial introduces denoising auto-encoders (dA) using Theano.
+This tutorial introduces denoising auto-encoders (dA) using Theano.
 
- Denoising autoencoders are the building blocks for SdA.
- They are based on auto-encoders as the ones used in Bengio et al. 2007.
- An autoencoder takes an input x and first maps it to a hidden representation
- y = f_{\theta}(x) = s(Wx+b), parameterized by \theta={W,b}. The resulting
- latent representation y is then mapped back to a "reconstructed" vector
- z \in [0,1]^d in input space z = g_{\theta'}(y) = s(W'y + b').  The weight
- matrix W' can optionally be constrained such that W' = W^T, in which case
- the autoencoder is said to have tied weights. The network is trained such
- that to minimize the reconstruction error (the error between x and z).
+Starter code from the deeplearning.net's denoising auto-encoder.
 
- For the denosing autoencoder, during training, first x is corrupted into
- \tilde{x}, where \tilde{x} is a partially destroyed version of x by means
- of a stochastic mapping. Afterwards y is computed as before (using
- \tilde{x}), y = s(W\tilde{x} + b) and z as s(W'y + b'). The reconstruction
- error is now measured between z and the uncorrupted input x, which is
- computed as the cross-entropy :
-      - \sum_{k=1}^d[ x_k \log z_k + (1-x_k) \log( 1-z_k)]
-
-
- References :
+References :
    - P. Vincent, H. Larochelle, Y. Bengio, P.A. Manzagol: Extracting and
    Composing Robust Features with Denoising Autoencoders, ICML'08, 1096-1103,
    2008
@@ -28,6 +11,7 @@
    Training of Deep Networks, Advances in Neural Information Processing
    Systems 19, 2007
 
+Barak Oshri, Nishith Khandwala
 """
 import csv
 import os
@@ -42,38 +26,7 @@ import theano
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
 
-#from utils import tile_raster_images
-
-# try:
-#     import PIL.Image as Image
-# except ImportError:
-#     import Image
-
-
 class dA(object):
-    """Denoising Auto-Encoder class (dA)
-
-    A denoising autoencoders tries to reconstruct the input from a corrupted
-    version of it by projecting it first in a latent space and reprojecting
-    it afterwards back in the input space. Please refer to Vincent et al.,2008
-    for more details. If x is the input then equation (1) computes a partially
-    destroyed version of x by means of a stochastic mapping q_D. Equation (2)
-    computes the projection of the input into the latent space. Equation (3)
-    computes the reconstruction of the input, while equation (4) computes the
-    reconstruction error.
-
-    .. math::
-
-        \tilde{x} ~ q_D(\tilde{x}|x)                                     (1)
-
-        y = s(W \tilde{x} + b)                                           (2)
-
-        x = s(W' y  + b')                                                (3)
-
-        L(x,z) = -sum_{k=1}^d [x_k \log z_k + (1-x_k) \log( 1-z_k)]      (4)
-
-    """
-
     def __init__(
         self,
         numpy_rng,
@@ -92,70 +45,17 @@ class dA(object):
         word_vectors=None,
         bvis=None
     ):
-        """
-        Initialize the dA class by specifying the number of visible units (the
-        dimension d of the input ), the number of hidden units ( the dimension
-        d' of the latent or hidden space ) and the corruption level. The
-        constructor also receives symbolic variables for the input, weights and
-        bias. Such a symbolic variables are useful when, for example the input
-        is the result of some computations, or when weights are shared between
-        the dA and an MLP layer. When dealing with SdAs this always happens,
-        the dA on layer 2 gets as input the output of the dA on layer 1,
-        and the weights of the dA are used in the second stage of training
-        to construct an MLP.
 
-        :type numpy_rng: numpy.random.RandomState
-        :param numpy_rng: number random generator used to generate weights
-
-        :type theano_rng: theano.tensor.shared_randomstreams.RandomStreams
-        :param theano_rng: Theano random generator; if None is given one is
-                     generated based on a seed drawn from `rng`
-
-        :type input: theano.tensor.TensorType
-        :param input: a symbolic description of the input or None for
-                      standalone dA
-
-        :type n_visible: int
-        :param n_visible: number of visible units
-
-        :type n_hidden: int
-        :param n_hidden:  number of hidden units
-
-        :type W: theano.tensor.TensorType
-        :param W: Theano variable pointing to a set of weights that should be
-                  shared belong the dA and another architecture; if dA should
-                  be standalone set this to None
-
-        :type bhid: theano.tensor.TensorType
-        :param bhid: Theano variable pointing to a set of biases values (for
-                     hidden units) that should be shared belong dA and another
-                     architecture; if dA should be standalone set this to None
-
-        :type bvis: theano.tensor.TensorType
-        :param bvis: Theano variable pointing to a set of biases values (for
-                     visible units) that should be shared belong dA and another
-                     architecture; if dA should be standalone set this to None
-
-
-        """
         self.n_hidden = n_hidden
         self.word_vectors = word_vectors
         self.n_dict = n_dict
         self.f = T.nnet.sigmoid
         self.input_size = input_size
 
-        # create a Theano random generator that gives symbolic random values
         if not theano_rng:
             theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
 
-        # note : W' was written as `W_prime` and b' as `b_prime`
-
         if not H1:
-            # W is initialized with `initial_W` which is uniformely sampled
-            # from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
-            # converted using asarray to dtype
-            # theano.config.floatX so that the code is runable on GPU
             initial_H1 = numpy.asarray(
                 numpy_rng.uniform(
                     low=-4 * numpy.sqrt(6. / (self.n_hidden)),
@@ -167,11 +67,6 @@ class dA(object):
             H1 = theano.shared(value=initial_H1, name='H1', borrow=True)
 
         if not H2:
-            # W is initialized with `initial_W` which is uniformely sampled
-            # from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
-            # converted using asarray to dtype
-            # theano.config.floatX so that the code is runable on GPU
             initial_H2 = numpy.asarray(
                 numpy_rng.uniform(
                     low=-4 * numpy.sqrt(6. / (self.n_hidden)),
@@ -183,11 +78,6 @@ class dA(object):
             H2 = theano.shared(value=initial_H2, name='H2', borrow=True)
 
         if not C:
-            # W is initialized with `initial_W` which is uniformely sampled
-            # from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
-            # converted using asarray to dtype
-            # theano.config.floatX so that the code is runable on GPU
             initial_C = numpy.asarray(
                 numpy_rng.uniform(
                     low=-4 * numpy.sqrt(6. / (self.n_hidden)),
@@ -199,11 +89,6 @@ class dA(object):
             C = theano.shared(value=initial_C, name='C', borrow=True)
 
         if not Y:
-            # W is initialized with `initial_W` which is uniformely sampled
-            # from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
-            # converted using asarray to dtype
-            # theano.config.floatX so that the code is runable on GPU
             initial_Y = numpy.asarray(
                 numpy_rng.uniform(
                     low=-4 * numpy.sqrt(6. / (self.n_hidden)),
@@ -215,11 +100,6 @@ class dA(object):
             Y = theano.shared(value=initial_Y, name='Y', borrow=True)
 
         if not S:
-            # W is initialized with `initial_W` which is uniformely sampled
-            # from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
-            # converted using asarray to dtype
-            # theano.config.floatX so that the code is runable on GPU
             initial_S = numpy.asarray(
                 numpy_rng.uniform(
                     low=-4 * numpy.sqrt(6. / (self.n_hidden)),
@@ -237,17 +117,17 @@ class dA(object):
         self.S = S
 
         self.theano_rng = theano_rng
-        # if no input is given, generate a variable representing the input
+
         if input is None:
-            # we use a matrix because we expect a minibatch of several
-            # examples, each example being a row
             self.x = T.dmatrix(name='input')
         else:
             self.x = input
 
         self.params = [self.H1, self.S, self.Y, self.C, self.H2]
 
-    #Input is a list of word vectors
+    def get_params(self):
+        return {'H1': self.H1, 'H2': self.H2, 'C': self.C, 'Y': self.Y, 'S': self.S}
+
     def get_hidden_values(self, sentence):
         self.input_count = 0
         h0 = theano.shared(value=np.zeros((self.n_hidden), dtype=theano.config.floatX))
@@ -270,8 +150,6 @@ class dA(object):
 
         return y, s
 
-        #return y
-
     def _decoder_recurrence(self, ht_1, yt_1, hidden):
         h_t = self.f(T.dot(self.H2, ht_1) + T.dot(self.Y, yt_1) + T.dot(self.C, hidden))
         a = T.dot(self.S, h_t)
@@ -285,28 +163,13 @@ class dA(object):
 
         #tilde_x = self.get_corrupted_input(self.x, corruption_level)
         hidden = self.get_hidden_values(self.x)
-        #hidden2 = self.get_hidden_values(self.x)
         output, softmaxes = self.get_reconstructed_input(hidden)
         output_hidden = self.get_hidden_values(output)
-        # note : we sum over the size of a datapoint; if we are using
-        #        minibatches, L will be a vector, with one entry per
-        #        example in minibatch
-        #cost = T.mean(hidden) - T.mean(output) + T.mean(output) - T.mean(output_hidden)
-        #cost = T.mean(hidden) - T.mean(output)
-
 
         cost = T.sqrt(T.sum(T.sqr(output_hidden - hidden))) + T.sum(T.nnet.categorical_crossentropy(softmaxes, self.x))
-        # note : L is now a vector, where each element is the
-        #        cross-entropy cost of the reconstruction of the
-        #        corresponding example of the minibatch. We need to
-        #        compute the average of all these to get the cost of
-        #        the minibatch
-        #cost = T.mean(L)
 
-        # compute the gradients of the cost of the `dA` with respect
-        # to its parameters
         gparams = T.grad(cost, self.params)
-        # generate the list of updates
+
         updates = [
             (param, param - learning_rate * gparam)
             for param, gparam in zip(self.params, gparams)
@@ -329,66 +192,51 @@ def build(src_filename, delimiter=',', header=True, quoting=csv.QUOTE_MINIMAL):
     return (np.array(mat), rownames, colnames)
 
 
-def test_dA(learning_rate=0.1, training_epochs=15,
-            dataset='mnist.pkl.gz', output_folder='dA_plots'):
+def train_dA(learning_rate=0.1, training_epochs=15,
+            dataset='mnist.pkl.gz', output_folder='dA_plots', params_dict = None):
 
-    """
-    This demo is tested on MNIST
-
-    :type learning_rate: float
-    :param learning_rate: learning rate used for training the DeNosing
-                          AutoEncoder
-
-    :type training_epochs: int
-    :param training_epochs: number of epochs used for training
-
-    :type dataset: string
-    :param dataset: path to the picked dataset
-
-    """
-    # List of 1-d word vector sentences
-    #X_train = [numpy.zeros((30)) for i in range(100)]
-    # compute number of minibatches for training, validation and testing
-    #n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-
-    # start-snippet-2
-    # allocate symbolic variables for the data
-    # index to a [mini]batch
-     # the data is presented as rasterized images
-    # end-snippet-2
-
-    # if not os.path.isdir(output_folder):
-    #     os.makedirs(output_folder)
-    # os.chdir(output_folder)
-
-    ####################################
-    # BUILDING THE MODEL NO CORRUPTION #
-    ####################################
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    os.chdir(output_folder)
 
     glove_matrix, glove_vocab, _ = build('glove.6B.50d.txt', delimiter=' ', header=False, quoting=csv.QUOTE_NONE)
     f = open("all_wvi.pkl", 'r')
     sentences = pickle.load(f)
     glove_matrix = theano.shared(value=np.array(glove_matrix, dtype=theano.config.floatX), borrow=True)
     X_train = map(lambda x: np.array(x), sentences[1:101])
-    #X_train = [np.zeros((20), dtype='int32'), np.zeros((20), dtype='int32')]
-    #X_train = map(lambda x: theano.shared(np.array(x, dtype = theano.config.floatX), borrow=True), X[:100])
-    #print X_train[0].type
 
     rng = numpy.random.RandomState(123)
     theano_rng = RandomStreams(rng.randint(2 ** 30))
     x = T.lvector('x')
     input_size = T.scalar(dtype='int64')
 
-    da = dA(
-        numpy_rng=rng,
-        theano_rng=theano_rng,
-        input=x,
-        wvec_dim=50,
-        n_hidden=50,
-        word_vectors=glove_matrix,
-        n_dict=len(glove_vocab),
-        input_size=input_size
-    )
+    if params_dict:
+        da = dA(
+            numpy_rng=rng,
+            theano_rng=theano_rng,
+            input=x,
+            wvec_dim=50,
+            n_hidden=50,
+            word_vectors=glove_matrix,
+            n_dict=len(glove_vocab),
+            input_size=input_size
+            H1=params_dict['H1']
+            H2=params_dict['H2']
+            C=params_dict['C']
+            Y=params_dict['Y']
+            S=params_dict['S']
+        )
+    else:
+        da = dA(
+            numpy_rng=rng,
+            theano_rng=theano_rng,
+            input=x,
+            wvec_dim=50,
+            n_hidden=50,
+            word_vectors=glove_matrix,
+            n_dict=len(glove_vocab),
+            input_size=input_size
+        )
 
     cost, updates = da.get_cost_updates(
         corruption_level=0.,
@@ -406,16 +254,23 @@ def test_dA(learning_rate=0.1, training_epochs=15,
 
     start_time = time.clock()
     for epoch in xrange(training_epochs):
-        c = []
+        cost_history = []
         for batch_index in xrange(100):
-            c.append(train_da(X_train[batch_index]))
-            print 'Iteration %d, mean cost %d' % (batch_index, numpy.mean(c))
+            cost_history.append(train_da(X_train[batch_index]))
+            print 'Iteration %d, mean cost %d' % (batch_index, numpy.mean(cost_history))
 
-        print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
+        print 'Training epoch %d, cost ' % epoch, numpy.mean(cost_history)
 
     end_time = time.clock()
-
     training_time = (end_time - start_time)
 
+    print 'Finished training %d epochs, took %d seconds' % (training_epochs, training_time)
+
+    return cost_history, da.get_params(), dA, train_da
+
+def test_dA(dA):
+    
+
 if __name__ == '__main__':
-    test_dA()
+    cost_history, params, dA, train_da = train_dA()
+    sample_sentences, cost, test_da = test_dA(dA)
